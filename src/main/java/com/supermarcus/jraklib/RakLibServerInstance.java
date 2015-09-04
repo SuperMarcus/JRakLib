9180b1af3925e77e99b15384d6abdaf48a25582a
SuperMarcus <zhouxule@icloud.com>
2015-09-04 13:12:05 +0800

    Add comments

--- src/main/java/com/supermarcus/jraklib/RakLibServerInstance.java
+++ src/main/java/com/supermarcus/jraklib/RakLibServerInstance.java
@@ -1,27 +1,48 @@
 package com.supermarcus.jraklib;
 
-import com.supermarcus.jraklib.lang.RawPacket;
+import com.supermarcus.jraklib.lang.exceptions.InterfaceOutOfPoolSizeException;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
 
+/**
+ * RakLib Server Instance
+ *
+ * - Extend this class in order to use JRakLib
+ */
 abstract public class RakLibServerInstance {
-    private LinkedBlockingQueue<RawPacket> packetQueue = new LinkedBlockingQueue<>();
-
     private SessionManager manager = new SessionManager();
-
-    public final void offerPacket(RawPacket raw){
-        this.getPacketQueue().offer(raw);
-    }
 
     protected SessionManager getSessionManager(){
         return manager;
     }
 
+    /**
+     * Add a interface
+     *
+     * @param address The address binding on
+     * @throws SocketException
+     * @throws InterfaceOutOfPoolSizeException
+     */
+    protected void addInterface(InetSocketAddress address) throws SocketException, InterfaceOutOfPoolSizeException {
+        this.getSessionManager().addInterface(address);
+    }
+
+    /**
+     * Add a handler which handle the messages triggered by JRakLib
+     *
+     * @param handler The handler which will be add to message handler list
+     */
     protected void addMessageHandler(MessageHandler handler){
         this.getSessionManager().addMessageHandler(handler);
     }
 
-    protected LinkedBlockingQueue<RawPacket> getPacketQueue(){
-        return this.packetQueue;
+    /**
+     * Set the handler which will handle all packets and encapsulated packets
+     *
+     * @param handler The handler which will be add to packet handler list
+     */
+    protected void setPacketHandler(PacketHandler handler){
+        this.getSessionManager().setPacketHandler(handler);
     }
 }
